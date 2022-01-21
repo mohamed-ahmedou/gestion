a8d2c3936cceff40a148128145f6c78d1b44b591
mohamed-ahmedou <mahmedou500@gmail.com>
2022-01-21 01:36:00 +0100

    sup mod ajou

--- Bourse/urls.py
+++ Bourse/urls.py
@@ -2,15 +2,20 @@ from unicodedata import name
 from django.urls import path
 from .views import *
 
+
 # urlpatterns = [
 #     path('',home, name='home')
 # ]
-
+ 
 from . import views
+
 urlpatterns = [ 
     path('', views.home, name="home"),
     path('voiture/', views.voitures, name="voiture"), 
     path('client/<str:pk>',views.client, name="client"), 
-     path('create',views.create, name="create"), 
+    path('ajouter',views.ajouter, name="ajouter"),
+    path('modifier/<str:pk>',views.modifier, name="modifier"),
+    path('confirmersupression/<int:pk>/',views.confirmersupression, name="confirmersupression"),
+    path('supprimer/<int:pk>/',views.supprimer, name="supprimer"),
     # path('Elouss6oura',views.elouss6oura),
 ]
--- Bourse/views.py
+++ Bourse/views.py
@@ -1,12 +1,12 @@
-from multiprocessing import context
-from sqlite3 import PARSE_COLNAMES
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from Bourse.forms import FactureForm
 
 from .models import * 
 from .forms import FactureForm
+
 def home(request):
     client = Client.objects.all()
     facture = Facture.objects.all()
@@ -34,7 +34,45 @@ def client(request,pk):
     
     return render(request, 'Bourse/Client.html', context)   
 
-def create(request):
+def ajouter(request):
    form = FactureForm()
+   if request.method == 'POST':
+       form = FactureForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect('/')
    context = {'form':form}
    return render(request, 'Bourse/mon_form.html',context)
+
+def modifier(request,pk):
+    facture = Facture.objects.get(id=pk)
+    form = FactureForm(instance=facture) 
+    if request.method == 'POST':
+           form = FactureForm(request.POST, instance=facture)
+           if form.is_valid():
+              form.save()
+              return redirect('/')
+    context = {'form':form}
+    return render(request, 'Bourse/mon_form.html',context)
+
+def confirmersupression(request,pk):
+    pk = pk 
+    return render(request, 'Bourse/supprimer_form.html',{'pk': pk})
+
+def supprimer(request,pk):
+    
+    facture = Facture.objects.filter(id=pk)
+    facture.delete()
+    
+    client = Client.objects.all()
+    facture = Facture.objects.all()
+    t_facture = facture.count()
+    a_facture = facture.filter(type='ACHAT').count()
+    v_facture = facture.filter(type='VENDRE').count() 
+    context = {'client':client,
+               'facture':facture,
+               't_facture':t_facture,
+               'a_facture':a_facture,
+               'v_facture':v_facture}
+    
+    return render(request, 'Bourse/tabledebord.html', context)
